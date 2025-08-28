b5efe9c8431062239ce226d7371ada53309b4ce0
Michael-Kirkland <michaelk.22082@obhs.school.nz>
2025-08-29 09:14:51 +1200

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+
+def sandwich_maker(options):
+
+
+bread_options = ["White", "Wholemeal", "Malted Rye", "Flatbread", "Italian"]
+meat_options = ["Pastrami", "Pepperoni", "Turkey", "Chicken", "Champaign Ham"]
+cheese_options = [""]
